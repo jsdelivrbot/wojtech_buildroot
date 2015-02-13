4729b11cd555410ea3d4b27459908f6a28037fc7
Pawel W <elendil@konto.pl>
2015-02-13 14:21:27 -0500

    Initial commit to new branch

---
+++ board/wojtech/common/post-build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+rm -rf $TARGET_DIR/etc/fstab
+rm -rf $TARGET_DIR/etc/mtab
+rm -rf $TARGET_DIR/etc/inputrc
+rm -rf $TARGET_DIR/etc/profile
+rm -rf $TARGET_DIR/etc/protocols
+rm -rf $TARGET_DIR/etc/services
+rm -rf $TARGET_DIR/etc/random-seed
+rm -rf $TARGET_DIR/etc/resolv.conf
+rm -rf $TARGET_DIR/etc/securetty
+
+
+return 0
---
+++ package/ecuApp/ecuApp.mk
@@ -0,0 +1,29 @@
+ECUAPP_SITE = git://git.assembla.com/lnxEmbdDevice.git
+ECUAPP_SITE_METHOD = git
+ECUAPP_VERSION = v1.01
+ECUAPP_SOURCE = ecuApp-$(ECUAPP_VERSION).tar.gz
+ECUAPP_INSTALL_STAGING = YES
+ECUAPP_INSTALL_TARGET = YES
+
+define ECUAPP_BUILD_CMDS
+	$(@D)/br2_config $(TARGET_CXX) $(@D)
+	$(MAKE) PROJECT_ROOT=$(@D) -C $(@D)/Src
+endef
+
+define ECUAPP_INSTALL_STAGING_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/Bin/Target/usr/bin/* $(STAGING_DIR)/usr/bin
+	$(INSTALL) -D -m 0755 $(@D)/Bin/Target/usr/lib/* $(STAGING_DIR)/usr/lib
+endef
+
+define ECUAPP_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/Bin/Target/usr/bin/* $(TARGET_DIR)/usr/bin
+	$(INSTALL) -D -m 0755 $(@D)/Bin/Target/usr/lib/* $(TARGET_DIR)/usr/lib
+	mkdir -p $(TARGET_DIR)/etc/opt
+	mkdir -p $(TARGET_DIR)/srv/www/cgi-bin
+	$(INSTALL) -D -m 0755 $(@D)/Configuration/* $(TARGET_DIR)/etc/opt
+	$(INSTALL) -D -m 0755 $(@D)/HTMLSource/* $(TARGET_DIR)/srv/www
+	rm -rf $(TARGET_DIR)/srv/www/cgi-bin/controller
+	ln -s '/usr/bin/cgiprocessor' $(TARGET_DIR)/srv/www/cgi-bin/controller
+endef
+
+$(eval $(generic-package))
