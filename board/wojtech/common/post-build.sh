fbf8ba6df1c1d0c02dea33e53b25d157baf697b7
Pawel W <elendil@konto.pl>
2015-04-03 15:17:28 -0400

    Last minute change - get it built

--- board/wojtech/common/post-build.sh
+++ board/wojtech/common/post-build.sh
@@ -6,7 +6,6 @@ rm -rf $TARGET_DIR/etc/inputrc
 rm -rf $TARGET_DIR/etc/profile
 rm -rf $TARGET_DIR/etc/protocols
 rm -rf $TARGET_DIR/etc/services
-rm -rf $TARGET_DIR/etc/random-seed
 rm -rf $TARGET_DIR/etc/resolv.conf
 rm -rf $TARGET_DIR/etc/securetty
 
