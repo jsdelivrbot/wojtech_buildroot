e6e1ff16965bf9a73a8e4da7093302e69d85fecd
Pawel W <elendil@konto.pl>
2015-11-12 18:20:41 -0500

    use the ecu version v1.16 (with Watchdog running)

--- package/ecuApp/ecuApp.mk
+++ package/ecuApp/ecuApp.mk
@@ -1,6 +1,6 @@
 ECUAPP_SITE = git://git.assembla.com/lnxEmbdDevice.git
 ECUAPP_SITE_METHOD = git
-ECUAPP_VERSION = v1.12
+ECUAPP_VERSION = v1.16
 ECUAPP_SOURCE = ecuApp-$(ECUAPP_VERSION).tar.gz
 ECUAPP_INSTALL_STAGING = YES
 ECUAPP_INSTALL_TARGET = YES
@@ -22,7 +22,7 @@ define ECUAPP_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/etc/opt
 	mkdir -p $(TARGET_DIR)/srv/www/cgi-bin
 	$(INSTALL) -D -m 0755 $(@D)/Configuration/* $(TARGET_DIR)/etc/opt
-	$(INSTALL) -D -m 0755 $(@D)/HTMLSource/* $(TARGET_DIR)/srv/www
+	cp -r $(@D)/HTMLSource/* $(TARGET_DIR)/srv/www
 	rm -rf $(TARGET_DIR)/srv/www/cgi-bin/controller
 	ln -s '/usr/bin/cgiprocessor' $(TARGET_DIR)/srv/www/cgi-bin/controller
 endef
