ba7d67a1afdac010c9032b9b35f1930c5f62880e
Pawel Wojtowicz <pawel.f.wojtowicz@gmail.com>
2016-11-19 13:38:12 -0500

    ECU app installed rougly OK- not tested

--- package/ecuApp/ecuApp.mk
+++ package/ecuApp/ecuApp.mk
@@ -1,18 +1,10 @@
 ECUAPP_SITE = https://github.com/pawelwojtowicz/ecuApp.git
 ECUAPP_SITE_METHOD = git
-ECUAPP_VERSION = v1.16.01
+ECUAPP_VERSION = v1.16.07
 ECUAPP_SOURCE = ecuApp-$(ECUAPP_VERSION).tar.gz
 ECUAPP_INSTALL_STAGING = YES
 ECUAPP_INSTALL_TARGET = YES
 ECUAPP_DEPENDENCIES = flite
-
-define ECUAPP_POST_INSTALL_TARGET_HOOKS
-	mkdir -p $(TARGET_DIR)/etc/opt
-	mkdir -p $(TARGET_DIR)/srv/www/cgi-bin
-	$(INSTALL) -D -m 0755 $(@D)/Configuration/* $(TARGET_DIR)/etc/opt
-	cp -r $(@D)/HTMLSource/* $(TARGET_DIR)/srv/www
-	rm -rf $(TARGET_DIR)/srv/www/cgi-bin/controller
-	ln -s '/usr/bin/cgiprocessor' $(TARGET_DIR)/srv/www/cgi-bin/controller
-endef
+ECUAPP_CONF_OPTS = -DECUAPP_INSTALL_ROOT=$(TARGET_DIR)
 
 $(eval $(cmake-package))
